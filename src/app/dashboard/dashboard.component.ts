2db1216960d707ae3c99c36463f0c1f1e2053028
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-07-21 00:06:36 +0200

    Observable subscriben mit async-Pipe

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { Book } from '../shared/book';
 import { BookStoreService } from '../book-store.service';
@@ -10,19 +11,17 @@ import { BookStoreService } from '../book-store.service';
 })
 export class DashboardComponent implements OnInit {
 
-  books: Book[];
+  books$: Observable<Book[]>;
   url = 'http://angular.schule';
 
   constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
-    this.bs
-      .getAll()
-      .subscribe(books => this.books = books);
+    this.books$ = this.bs.getAll();
   }
 
   reorderBooks(book: Book) {
-    this.books.sort((a, b) => b.rating - a.rating);
+    // this.books.sort((a, b) => b.rating - a.rating);
   }
 
 }
